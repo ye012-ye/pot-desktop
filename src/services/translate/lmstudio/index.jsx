1d859f2fea7fb5da9f7d29cd7db3856c6306f36b
ye_wj <yewj246@gmail.com>
2026-06-22 11:42:06 +0800

    feat: add LM Studio translation adapter

---
+++ src/services/translate/lmstudio/index.jsx
@@ -0,0 +1,11 @@
+import { translate as translateWithOpenAI } from '../openai';
+import { toOpenAIConfig } from './api';
+
+export async function translate(text, from, to, options = {}) {
+    return translateWithOpenAI(text, from, to, {
+        ...options,
+        config: toOpenAIConfig(options.config),
+    });
+}
+
+export * from './info';
